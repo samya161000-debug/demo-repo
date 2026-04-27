fb9b13d907aa6c4f976eb76ef482b0027e7a950d
Samya Arora <samya161000@gmail.com>
2026-04-27 13:25:45 +0530

    added tsp code

--- tsp.cpp
+++ tsp.cpp
@@ -1,13 +1,48 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int tsp[10][10]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
-int visited[10],n,cost =0;
-void ts(int c){
-int k,adj_vertex=999;
-int min=999;
-visited[c]=1;
-cout<<c+1<<" ";
+int n;
+int dist[20][20];
+
+int dp[20][1 << 20];
+
+// mask → visited cities
+// pos → current city
+
+int tsp(int pos, int mask) {
+    if (mask == (1 << n) - 1) {
+        return dist[pos][0]; // return to start
+    }
+
+    if (dp[pos][mask] != -1)
+        return dp[pos][mask];
+
+    int ans = INT_MAX;
+
+    for (int city = 0; city < n; city++) {
+        if (!(mask & (1 << city))) {
+            ans = min(ans,
+                      dist[pos][city] +
+                      tsp(city, mask | (1 << city)));
+        }
+    }
+
+    return dp[pos][mask] = ans;
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> dist[i][j];
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << "Minimum cost of TSP: "
+         << tsp(0, 1) << endl;
+
+    return 0;
 }
 
 
